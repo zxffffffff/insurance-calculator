e2c8ef0663e20ea791b1916ea318364d516ea823
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2024-08-20 22:59:44 +0800

    npm install antd --save

--- web/src/index.js
+++ web/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={{ token: { colorPrimary: '#61dafb' } }}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
