8f41aa6a4428132c060656a7ca5990d28ec7323b
xiaofeng.zhu <34299738+zxffffffff@users.noreply.github.com>
2024-08-21 09:27:46 +0800

    npm install @ant-design/charts --save

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'antd';
+import { Line } from '@ant-design/charts';
 
 function App() {
+  const data = [
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ];
+  const config = {
+    data,
+    height: 400,
+    xField: 'year',
+    yField: 'value',
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +39,7 @@ function App() {
           Learn React
         </a>
         <Button type="primary">Button</Button>
+        <Line {...config} />
       </header>
     </div>
   );
